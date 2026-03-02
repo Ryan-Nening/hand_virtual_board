7a4742038ba0999bd2064a751e021215fa8d5141
Ryan-Nening <neningryan10@gmail.com>
2026-03-03 04:30:00 +0800

    generated 20x20 floor and added first person controller

--- hand_tracking_voxel_builder.py
+++ hand_tracking_voxel_builder.py
@@ -11,4 +11,18 @@ mediapipe_hands = mediapipe.solutions.hands
 hand_tracker = mediapipe_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 previous_pinch_state = False
 
+class VoxelBlock(Button):
+
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(parent=scene, position=position, model='cube', origin_y=0.5, texture='white_cube', color=color.color(0, 0, 1))
+
+    def input(self, key_press):
+        if self.hovered:
+            if key_press == 'left mouse down':
+                new_voxel_position = self.position + mouse.normal
+                voxel_block = VoxelBlock(position=new_voxel_position)
+
+            if key_press == 'right mouse down':
+                destroy(self)
+                
 application_engine.run()
